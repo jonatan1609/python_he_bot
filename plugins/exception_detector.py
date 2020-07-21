401cea3f143bd366905ca7583a7fdda657a6eec4
Jonatan <5377074j@gmail.com>
2020-07-22 00:34:08 +0300

    Version 3.0.0

--- plugins/exception_detector.py
+++ plugins/exception_detector.py
@@ -5,7 +5,7 @@ from .utils import detect_exception, groups
 @Client.on_message(~Filters.private & Filters.text & Filters.create(lambda _, m: all(x in m.text for x in (
             'Traceback (most recent call last):',
             'File', 'line', 'in', '.py'
-    )) and len(m.text) >= 250) & Filters.create(lambda _, m: str(m.chat.id) in groups))
+    )) and len(m.text) > 250) & Filters.create(lambda _, m: str(m.chat.id) in groups))
 async def handle_exception(_client: Client, message: Message):
     text = await detect_exception(message.text)
     await message.reply(
