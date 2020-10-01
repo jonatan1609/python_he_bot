b69296f34728881d49c713c7cc13e019fa8e70fa
jonatan1609 <49692607+jonatan1609@users.noreply.github.com>
2020-10-01 23:46:48 +0300

    fix SyntaxError

--- plugins/paste.py
+++ plugins/paste.py
@@ -2,7 +2,7 @@ from pyrogram import Client, Filters, Message
 from .utils import detect_exception
 
 
-@Client.on_message(Filters.reply & 'paste', '#'))
+@Client.on_message(Filters.reply & Filters.command('paste', '#'))
 async def paste_code(_client: Client, message: Message):
     await message.reply(await detect_exception(message.reply_to_message.text))
    
