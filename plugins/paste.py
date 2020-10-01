02c232547d0ea9d62611116fa91e01bc31d82766
jonatan1609 <49692607+jonatan1609@users.noreply.github.com>
2020-10-01 23:50:12 +0300

    Update paste.py

--- plugins/paste.py
+++ plugins/paste.py
@@ -4,5 +4,6 @@ from .utils import detect_exception
 
 @Client.on_message(Filters.reply & Filters.command('paste', '#'))
 async def paste_code(_client: Client, message: Message):
-    await message.reply(await detect_exception(message.reply_to_message.text))
+    await message.delete()
+    await message.reply_to_message.reply(await detect_exception(message.reply_to_message.text))
    
