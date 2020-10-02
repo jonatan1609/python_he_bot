713ff6981a3ae803f70288f384b0f5ffc09b6537
asher morgenstern <ashermorgenstern4@gmail.com>
2020-10-02 17:27:12 +0300

    set paste command permissions

--- plugins/paste.py
+++ plugins/paste.py
@@ -4,6 +4,15 @@ from .utils import detect_exception
 
 @Client.on_message(Filters.reply & Filters.command('paste', '#'))
 async def paste_code(_client: Client, message: Message):
+
     await message.delete()
-    await message.reply_to_message.reply(await detect_exception(message.reply_to_message.text))
+
+    if commander_status in ('creator', 'administrator') or original_message_sender_id == commander_id:
+
+        original_message_sender_id = message.reply_to_message.from_user.id
+        commander_id = message.from_user.id
+        commander_status = await (_client.get_chat_member(message.chat.id, commander_id)).status
+
+        await message.reply_to_message.delete()
+        await (message.reply_to_message.reply(await detect_exception(message.reply_to_message.text)))
    
