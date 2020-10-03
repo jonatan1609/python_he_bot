be0a6ed016c11da8b66fae27b277308b8e246266
jonatan <5377074j@gmail.com>
2020-10-03 22:11:24 +0300

    add #refresh command

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from pyrogram import Client, idle
+from pyrogram import Client, idle, handlers, filters
 from plugins.utils import set_administrators
 
 
 app = Client("PythonHeBot")
+app.add_handler(handlers.MessageHandler(set_administrators, filters.command('refresh', '#')))
 app.start()
 app.loop.run_until_complete(set_administrators(app))
 idle()
--- plugins/utils.py
+++ plugins/utils.py
@@ -9,14 +9,13 @@ bot_username = config['bot']['username'].strip('"')
 admins = {}
 
 
-async def set_administrators(app):
+async def set_administrators(app, message=None):
     global admins
-    for chat in groups:
-        async for admin in app.iter_chat_members(chat, filter="administrators"):
-            if admins.get(chat):
-                admins[chat].append(admin.user.id)
-            else:
-                admins[chat] = [admin.user.id]
+    if not message or message.from_user.id in admins.get(message.chat.id):
+        for chat in groups:
+            admins[chat] = [admin.user.id async for admin in app.iter_chat_members(chat, filter="administrators")]
+        return message.reply("Admins list updated successfully.") if message else ...
+    return message.reply("You have no permission to update the admins list.")
 
 
 async def detect_exception(string):
